7eff6c6ae1a0298fcff5611b347e52dd4b7e2bbe
FeiShian Tzeng <51775235+Tzeng0817@users.noreply.github.com>
2019-09-10 14:38:53 -0500

    BatterShip cout updated !!

--- Boards.cpp
+++ Boards.cpp
@@ -1,106 +1,55 @@
-#include "Boards.h"
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <stdexcept>
+#include "Executive.h"
+#include "Boards.h"
 
-Boards::Boards(){
-  /*Default values put into rows and columns.*/
-  rows = 8;
-  cols = 8;
-  /*Creating the board, implemented as a 2-D array of
-    characters on the heap.*/
-  for(int i = 0 ; i < rows ; i++) {
-    for(int j = 0; j < cols; j++){
-      offensiveBoard[i][j] = '~';
-      defensiveBoard[i][j] = '~';
-    }
-  }
+Executive::Executive(){
+
 }
-//Converts the letter for column into a number
-int Boards::charConvert(char temp){
-  int columnNumber = 0;
-  if(temp == 'A' || temp == 'a'){
-    columnNumber = 0;
-  }
-  if(temp == 'B' || temp == 'b'){
-    columnNumber = 1;
-  }
-  if(temp == 'C' || temp == 'c'){
-    columnNumber = 2;
-  }
-  if(temp == 'D' || temp == 'd'){
-    columnNumber = 3;
-  }
-  if(temp == 'E' || temp == 'e'){
-    columnNumber = 4;
-  }
-  if(temp == 'F' || temp == 'f'){
-    columnNumber = 5;
-  }
-  if(temp == 'G' || temp == 'g'){
-    columnNumber = 6;
-  }
-  if(temp == 'H' || temp == 'h'){
-    columnNumber = 7;
-  }
-  return columnNumber;
+Executive::~Executive(){
 }
-bool Boards::isValid(char column, int row){
-  int columnNumber = charConver(column);  
-  if((columnNumber <= 7 && columnNumber >= 0) && (row <= 7 && row >= 0)){
-    if(defensiveBoard[columnNumber][row] == '~'){
-      return true;
-    }
+void Executive::run(){
+  int choice=0;
+std::cout<< " ____        _   _   _      ____  _     _\n"<<"| __ )  __ _| |_| |_| | ___/ ___|| |__ (_)_ __\n"<<"|  _ \\ / _` | __| __| |/ _ \\___ \\| '_ \\| | '_ \\\n"<<"| |_) | (_| | |_| |_| |  __/___) | | | | | |_) |\n"<<"|____/ \\__,_|\\__|\\__|_|\\___|____/|_| |_|_| .__/\n"<<"                                        |_|\n";
+std::cout<<"1)START GAME"<<"\n";
+std::cout<<"2)EXIT"<<"\n";
+std::cin>>choice;
+if(choice==1){
+Boards* newBoard = new Boards();
+newBoard->displayOffensiveBoard();
+newBoard->displayOffensiveBoard();
+newBoard->displayBoth();
+ int row = 0;
+ char column = ' ';
+ std::cout << "\nEnter the column letter: ";
+ std::cin >> column;
+ while(column < 65 || column > 72){
+   if(column < 65 || column > 72){
+   std::cout << "Invalid selection, try again\n";
+   std::cout << "Enter the column letter: ";
+   std::cin >> column;
   }
-  return false;
-}
-Boards::~Boards(){
-  //Deletes the allocated memory for the board.
-  for(int i = 0 ; i < rows ; i++) {
-    for(int j = 0 ; j < rows ; j++) {
-      offensiveBoard[i][j] = ' ';
-      defensiveBoard[i][j] = ' ';
-    }
-  }
-  delete offensiveBoard;
-  delete defensiveBoard;
-}
-//Displays both the offensive and defensive boards for a player
-void Boards::displayBoth() const{
-  cout << "\nOffensive board: \n";
-  displayOffensiveBoard();
-  cout << "\nDefensive board: \n";
-  displayDefensiveBoard();
-}
-//Displays the current offensive board
-void Boards::displayOffensiveBoard() const{
-  cout << "  A B C D E F G H\n";
-  for(int i = 0; i < rows; i++) {
-    cout << i << " ";
-    for(int j = 0; j < 8; j++) {
-      cout <<  offensiveBoard[i][j] << " ";
-    }
-    cout << "\n";
-  }
-}
-//Displays the current defensive board
-void Boards::displayDefensiveBoard() const{
-  cout << "  A B C D E F G H\n";
-  for(int i = 0; i < rows; i++) {
-    cout << i << " ";
-    for(int j = 0; j < 8; j++) {
-      cout <<  defensiveBoard[i][j] << " ";
-    }
-    cout << "\n";
-  }
-}
-//Checks the location in the array for 'S' and returns true
-//If that position is '~', 'M', or 'H' returns false
-bool Boards::isHit(char column, int row){
- int columnNumber = charConvert(column);
- if((columnNumber <= 7 && columnNumber >= 0) && (row <= 7 && row >= 0)){
-  if(defensiveBoard[columnNumber][row] == 'S'){
-    return true;
+  else{
+    std::cin >> column;
   }
  }
- return false;
+ std::cout << "\nEnter the row number: ";
+ std::cin >> row;
+ while (std::cin.fail() || row > 7 || row < 0){
+   std::cin.clear();
+   std::cin.ignore(INT8_MAX, '\n');
+   std::cout << "Invalid selection, try again.\n";
+   std::cout << "\nEnter the row number: ";
+   std::cin >> row;
+ }
+ if(newBoard->isValid(column, row)){
+   std::cout << "Is valid..\n";
+ }
+ else{
+   std::cout << "Invalid location\n";
+ }
+}
+else if(choice==2){
+}
 }
